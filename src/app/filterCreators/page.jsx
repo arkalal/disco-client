82fd547dc1ec7fd0204e322c134bff4498d3fe37
arkalal <arkalal.123@gmail.com>
2025-09-08 17:48:56 +0530

    fixing deploy

--- src/app/filterCreators/page.jsx
+++ src/app/filterCreators/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { Suspense } from 'react'
 import FilterCreatorsUI from "../../../components/filterCreators/FilterCreatorsUI"
 
 export default function FilterCreators() {
   return (
     <div className="filter-creators-page">
-      <FilterCreatorsUI />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FilterCreatorsUI />
+      </Suspense>
     </div>
   )
 }
